9cc58e41e17f5372f50ae23f08a154818a4e0e3a
Kooreis <kooreis.official@gmail.com>
2023-12-17 05:58:35 -0600

    In this update, the script checks if any arguments were passed to the program. If no arguments were passed, it prints a message asking for a file path and then exits.

--- File_Line_Counter_with_Word_Stats.cs
+++ File_Line_Counter_with_Word_Stats.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 using System.IO;
 using System.Linq;
@@ -12,27 +11,5 @@ class Program
             Console.WriteLine("Please provide a file path.");
             return;
         }
-
-        string filePath = args[0];
-
-        if (!File.Exists(filePath))
-        {
-            Console.WriteLine("File does not exist.");
-            return;
-        }
-
-        string[] lines = File.ReadAllLines(filePath);
-        int lineCount = lines.Length;
-
-        int wordCount = 0;
-        foreach (string line in lines)
-        {
-            string[] words = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-            wordCount += words.Length;
-        }
-
-        Console.WriteLine("Line Count: " + lineCount);
-        Console.WriteLine("Word Count: " + wordCount);
     }
 }
-```
