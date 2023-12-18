11ea9540717b864cf13d0b0b0301102c7125fec6
Kooreis <kooreis.official@gmail.com>
2023-12-18 04:58:35 -0600

    In this update, the BufferedReader is assigned to read from the file at the provided path. A line counter and a map to count the occurrences of each word are initialized.

--- File_Line_Counter_with_Word_Stats.java
+++ File_Line_Counter_with_Word_Stats.java
@@ -1,15 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-public class FileLineCounter {
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Usage: java FileLineCounter <file path>");
-            System.exit(0);
-        }
-
-        String filePath = args[0];
-        BufferedReader reader;
+try {
+            reader = new BufferedReader(new FileReader(filePath));
+            String line = reader.readLine();
+            int lineCount = 0;
+            Map<String, Integer> wordCount = new HashMap<>();
